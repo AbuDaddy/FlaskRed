c2e3b95c0bc2bef9186c2fe27df35cf353c0b8c7
AbuDaddy <abbamba06@gmail.com>
2024-06-11 10:21:47 +0200

    FlaskBlue project turned to FlaskRed

---
+++ flaskred/routes.py
@@ -0,0 +1,10 @@
+from flaskred import app
+from flask import render_template, url_for
+
+@app.route('/')
+def home():
+    return render_template('home.html', title='Home')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
